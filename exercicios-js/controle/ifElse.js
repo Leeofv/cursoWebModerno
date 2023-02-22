81f214ef308a2bbf3f3bfcd46fc3f29942f3d59d
Leonardo Fontes <leeofv@gmail.com>
2023-02-22 10:35:55 -0300

    Estrutura de controle IF

---
+++ exercicios-js/controle/ifElse.js
@@ -0,0 +1,11 @@
+const imprimirResultado = function(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(5)
+imprimirResultado('Epa!')//Cuidado!!!

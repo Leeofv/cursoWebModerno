ba847081c2e6a49009ab075377170ef6e511c861
Leonardo Fontes <leeofv@gmail.com>
2023-02-28 09:58:59 -0300

    Create arrowFunction3.js

---
+++ exercicios-js/funcao/arrowFunction3.js
@@ -0,0 +1,18 @@
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)//false pq o this não aponta mais para o global
+comparaComThis(obj)// Agora aponta para o 'obj' por conta do 'bind'
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)

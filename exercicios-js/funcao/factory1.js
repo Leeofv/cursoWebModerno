1ebea81ae8c7e62e1bdf96dd1da5f8c22124f41e
Leonardo Fontes <leeofv@gmail.com>
2023-03-02 11:20:57 -0300

    Create factory1.js

---
+++ exercicios-js/funcao/factory1.js
@@ -0,0 +1,12 @@
+//Factory é uma função que no final ela retorna um objeto
+
+//Factory simples
+function criarPessoa(){
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
+

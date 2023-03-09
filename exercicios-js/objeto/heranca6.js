e26bdb9a3ec1e74baee47de86d85dc9fcf460227
Leonardo Fontes <leeofv@gmail.com>
2023-03-09 13:08:29 -0300

    Create heranca6.js

---
+++ exercicios-js/objeto/heranca6.js
@@ -0,0 +1,19 @@
+function Aula(nome, videoID) {
+    this.nome = nome
+    this.videoID = videoID
+}
+
+const aula1 = new Aula('Bem vindo', 123)
+const aula2 = new Aula('Até breve', 456)
+console.log(aula1, aula2)
+
+//simulando o new
+function novo(f, ...params) {
+    const obj = {}
+    obj.__proto__ = f.prototype //__proto__ é o prototipo do objeto 'obj' apontando para a "f" função
+    f.apply(obj, params)
+    return obj
+}
+const aula3 = novo(Aula, 'Bem vindo', 123)
+const aula4 = novo(Aula, 'Até breve', 456)
+console.log(aula3, aula4)

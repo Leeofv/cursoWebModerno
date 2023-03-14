d919d45f152d8a6ecb30749879c8efa49292dfec
Leonardo Fontes <leeofv@gmail.com>
2023-03-14 09:02:33 -0300

    Create classe2.js

---
+++ exercicios-js/objeto/classe2.js
@@ -0,0 +1,21 @@
+class Avo {
+    constructor(sobrenome) {
+        this.sobrenome = sobrenome
+    }
+}
+
+class Pai extends Avo {
+    constructor(sobrenome, profissao = 'Professor') {
+        super(sobrenome) //o "super" chama a "super classe" que é o Avo
+        this.profissao = profissao
+    }
+}
+
+class Filho extends Pai {
+    constructor() {
+        super('Silva')
+    }
+}
+
+const filho = new Filho
+console.log(filho)

05134e17794440c7d2f7d499ebf45838ea5fa116
Leonardo Fontes <leeofv@gmail.com>
2023-04-03 13:10:06 -0300

    Create ex30.js

---
+++ exercicios1-js/ex30.js
@@ -0,0 +1,33 @@
+/* Escreva um algoritmo que percorre um vetor de inteiros e defina o maior e menor valor dentro do vetor.  */
+
+const vetor = [44, 66, 77, 88, 99]
+const min = vetor.map(Number).reduce(function(a, b){
+    return Math.min(a, b)
+})
+const max = vetor.map(Number).reduce(function(a, b){
+    return Math.max(a, b)
+})
+console.log(min, max)
+
+function maiorMenor (vetor) {
+    let maior
+    let menor
+    for (let i = 0; i < vetor.length; i++){
+        if (maior === undefined && menor === undefined){
+            maior = vetor[i]
+            menor = vetor[i]
+        } else {
+            if (vetor[i] > maior){
+                maior = vetor[i]
+            }
+            if(vetor[i] < menor){
+                menor = vetor[i]
+            }
+        }
+    }
+    return [maior, menor]
+}
+
+vetor =  [10, 5, 7, 3, 1, 3, 11, 20, 6, 9]
+
+console.log(maiorMenor(vetor))
